1a5354ee2c82001bf01ac6828d6293dce579ed8d
Srujana Chary <srujanarailsdev@gmail.com>
2013-01-16 19:05:51 -0600

    a

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ActiveRecord::Base
   attr_accessible :address, :city, :name, :state, :type, :zipcode
 end
+
+class Doctor < Profile
+  belongs_to :user
+end
+
+class Receptionist < Profile
+  belongs_to :user
+end
+
+class Customer < Profile
+  belongs_to :user
+  has_many :pets
+end
